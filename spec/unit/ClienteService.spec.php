62c26c7a012ba10e35817bb30cac70e6389f2d60
Arturoliveira1212 <arturalvesdeoliveira28@gmail.com>
2024-10-05 17:45:07 -0300

    Test: Inicia testes unitários para ClienteService.

---
+++ spec/unit/ClienteService.spec.php
@@ -0,0 +1,94 @@
+<?php
+
+use app\builders\ClienteBuilder;
+use app\dao\DAOEmBDR;
+use app\exceptions\ServiceException;
+use app\models\Cliente;
+use app\services\ClienteService;
+use app\services\Service;
+
+describe( 'ClienteService', function(){
+    beforeAll( function(){
+        $this->dao = Mockery::mock(DAOEmBDR::class);
+        $this->service = new ClienteService($this->dao);
+    } );
+
+    describe( 'Salvar', function(){
+        function validarMensagemAoSalvar( Service $service, Cliente $cliente, string $campoErro, string $mensagemErro ){
+            try {
+                $service->salvar($cliente);
+            } catch( ServiceException $e){
+                $erro = json_decode($e->getMessage(), true);
+                expect( $erro )->not->toBeEmpty();
+                expect( $erro )->toContainKey( $campoErro );
+                expect( $erro[ $campoErro ] )->toEqual( $mensagemErro);
+            }
+        }
+
+        it('Lança exceção ao não enviar nome válido para cliente', function() {
+            $this->dao->shouldReceive('existe')->andReturn(false);
+
+            $cliente = ClienteBuilder::novo()->comId(1)->comNome('')->comCpf('354.769.850-26')->comDataNascimento( new DateTime('2000-01-01'))->build();
+
+            validarMensagemAoSalvar( $this->service, $cliente, 'nome', 'Preencha o campo nome.' );
+        });
+
+        it('Lança exceção ao enviar nome com tamanho maior que o permitido', function() {
+            $this->dao->shouldReceive('existe')->andReturn(false);
+
+            $cliente = ClienteBuilder::novo()->comId(1)->comNome(str_repeat('a', Cliente::TAMANHO_MAXIMO_NOME + 1))->comCpf('354.769.850-26')->comDataNascimento( new DateTime('2000-01-01'))->build();
+
+            validarMensagemAoSalvar( $this->service, $cliente, 'nome', 'O campo nome deve ter entre ' . Cliente::TAMANHO_MINIMO_NOME . ' e ' . Cliente::TAMANHO_MAXIMO_NOME . ' caracteres.' );
+        });
+
+        it('Salva categoria com sucesso ao enviar nome com tamanho igual ao permitido', function() {
+            $this->dao->shouldReceive('existe')->andReturn(false);
+
+            $cliente = ClienteBuilder::novo()->comId(1)->comNome(str_repeat('a', Cliente::TAMANHO_MAXIMO_NOME))->comCpf('354.769.850-26')->comDataNascimento( new DateTime('2000-01-01'))->build();
+
+            $idCadastrado = 1;
+            $this->dao->shouldReceive('salvar')->andReturn($idCadastrado);
+
+            $resultado = $this->service->salvar($cliente);
+            expect($resultado)->toEqual($idCadastrado);
+        });
+
+        it( 'Lança exceção ao enviar cpf vazio', function(){
+            $cliente = ClienteBuilder::novo()->comId(1)->comNome('Nome válido')->comDataNascimento( new DateTime('2000-01-01'))->build();
+
+            validarMensagemAoSalvar( $this->service, $cliente, 'cpf', 'Preencha o campo cpf.' );
+        } );
+
+        it( 'Lança exceção ao enviar cpf no formato inválido', function(){
+            $cliente = ClienteBuilder::novo()->comId(1)->comNome('Nome válido')->comCpf('181')->comDataNascimento( new DateTime('2000-01-01'))->build();
+
+            validarMensagemAoSalvar( $this->service, $cliente, 'cpf', 'Formato inválido.' );
+        } );
+
+        it( 'Lança exceção ao enviar cpf no formato válido mas com números repetidos', function(){
+            $cliente = ClienteBuilder::novo()->comId(1)->comNome('Nome válido')->comCpf('111.111.111-11')->comDataNascimento( new DateTime('2000-01-01'))->build();
+
+            validarMensagemAoSalvar( $this->service, $cliente, 'cpf', 'Cpf inválido.' );
+        } );
+
+        it( 'Lança exceção ao enviar cpf já cadastrado no sistema', function(){
+            $this->dao->shouldReceive('existe')->andReturn(true);
+
+            $cliente = ClienteBuilder::novo()->comId(1)->comNome('Nome válido')->comCpf('354.769.850-26')->comDataNascimento( new DateTime('2000-01-01'))->build();
+
+            validarMensagemAoSalvar( $this->service, $cliente, 'cpf', 'Cpf já cadastrado no sistema.' );
+        } );
+    } );
+
+    describe( 'ObterComId', function(){
+
+    } );
+
+    describe( 'ObterComRestricoes', function(){
+
+    } );
+
+    describe( 'DesativarComId', function(){
+
+    } );
+} );
